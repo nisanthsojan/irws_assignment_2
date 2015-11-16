8b70aff827ebb964a2298a93ce9ef2a0c38b09b1
nisanthsojan <nisanthsojan@yahoo.com>
2015-11-16 20:56:54 +0000

    added function to find the unique term count

--- routes/counter.js
+++ routes/counter.js
@@ -2,6 +2,7 @@ var express = require('express');
 var router = express.Router();
 var _D = require('lodash');
 var S = require('string');
+var async = require('async');
 
 /*
  This function takes test string and do the following operations
@@ -111,7 +112,122 @@ router.get('/top/:name', function (req, res, next) {
     });
 });
 
-router.get('/unique', function (req, res, next) {
+/*
+ this function gets the corpus array and return the unique words in each corpus
+ */
+function getUniqueTerms(req, res, next) {
+    var uniqueWord = [];
+    // Array to hold async tasks
+    var asyncTasks = [];
+    var corpusDocuments = req.app.locals.corpuses;
+
+    _D.forEach(corpusDocuments, function (corpus, corpusIndex) {
+
+        asyncTasks.push(function (callback) {
+
+            var corpusesWordsString = '';
+            _D.each(corpusDocuments, function (corpuss, index) {
+                if (index != corpusIndex) {
+                    corpusesWordsString += ' ' + cleanText(corpuss.data);
+                }
+            });
+            var tmp = {
+                name: corpus.name,
+                words: [],
+                count: 0
+            };
+
+
+            var asyncTasks2 = [];
+            var corpusWords = _D.uniq(cleanText(corpus.data).split(' '));
+            var chunks = _D.chunk(corpusWords, 10);
+
+            _D.forEach(chunks, function (chunksChunks) {
+                asyncTasks2.push(function (chunksCallback) {
+                    _D.forEach(chunksChunks, function (word) {
+                        if (!S(corpusesWordsString).contains(word)) {
+                            tmp.words.push(word);
+                        }
+                    });
+                    return chunksCallback();
+                });
+            });
+
+            async.parallel(asyncTasks2, function (err) {
+                tmp.count = tmp.words.length;
+                uniqueWord.push(tmp);
+                return callback();
+            });
+        });
+
+    });
+
+
+    async.parallel(asyncTasks, function (err) {
+        req.UniqueWord = uniqueWord;
+        return next();
+    });
+}
+
+/*
+ this route counts the number of documents in each documents
+ */
+router.get('/unique', getUniqueTerms, function (req, res, next) {
+    res.render('unique', {
+        title: req.app.locals.title,
+        fileData: req.UniqueWord
+    });
+
+});
+
+/*
+a middleware to get the unique word count in each documents
+the unique words are computed from the 'getUniqueTerms' function which is stored in
+request variable 'req.UniqueWord' and modifies it to have count with them
+ */
+function getUniqueTermsCount(req, res, next) {
+    var asyncTasks = [];
+    _D.forEach(req.UniqueWord, function (uniqWordDoc) {
+        asyncTasks.push(function (uniqWordCallback) {
+
+            var chunks = _D.chunk(uniqWordDoc.words, 10);
+            var corpusDoc = _D.findWhere(req.app.locals.corpuses, {'name': uniqWordDoc.name});
+            var asyncTasks2 = [];
+            uniqWordDoc.words = [];
+
+            var cleanedData = cleanText(corpusDoc.data);
+
+            _D.forEach(chunks, function (chunksChunks) {
+                asyncTasks2.push(function (chunksCallback) {
+                    _D.forEach(chunksChunks, function (word) {
+                        uniqWordDoc.words.push({
+                            text: word,
+                            count: S(cleanedData).count(word)
+                        });
+                    });
+                    return chunksCallback();
+                });
+            });
+
+            async.parallel(asyncTasks2, function (err) {
+                return uniqWordCallback();
+            });
+        });
+    });
+
+    async.parallel(asyncTasks, function (err) {
+        //req.UniqueWord = uniqueWord;
+        return next();
+    });
+
+}
+
+router.get('/unique/count', getUniqueTerms, getUniqueTermsCount, function (req, res, next) {
+
+    res.render('unique_count', {
+        title: req.app.locals.title,
+        fileData: req.UniqueWord
+    });
 
 });
 
